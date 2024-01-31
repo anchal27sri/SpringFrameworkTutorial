797c79d46532d9214ac0ea832ab796d3926c8bf7
anchal27sri <srivastavaanchal000@gmail.com>
2024-01-31 23:58:26 +0530

    using application context and property initialization using spring

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,15 +1,18 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Triangle triangle = new Triangle();
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+
+		Triangle triangle = (Triangle) applicationContext.getBean("triangle");
 		triangle.draw();
 	}
 
--- src/main/java/com/example/demo/Triangle.java
+++ src/main/java/com/example/demo/Triangle.java
@@ -2,7 +2,17 @@ package com.example.demo;
 
 public class Triangle {
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    private String type;
+
     public void draw() {
-        System.out.println("Drawing Triangle");
+        System.out.println("Drawing" + getType() + "Triangle");
     }
 }
